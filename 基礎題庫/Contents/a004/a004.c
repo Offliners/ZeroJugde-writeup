af79f1b9c755fc158542ba27d3c9d47b57fd3a86
Offliner <42777325+Offliners@users.noreply.github.com>
2019-03-10 23:52:39 +0800

    Update a004.c

--- 基礎題庫/Contents/a004/a004.c
+++ 基礎題庫/Contents/a004/a004.c
@@ -1,1 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+  int year;
+  while(scanf("%d",&year)!=EOF)
+  {	
+    _Bool IsLeapYear = (year%400 == 0 || (year%4 == 0 && year%100 !=0));
+    if(IsLeapYear) 
+      printf("閏年\n",year);
+    else
+      printf("平年\n",year);
+  }
+  return 0;
 
