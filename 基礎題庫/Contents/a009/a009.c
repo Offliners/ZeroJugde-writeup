2a10dbd787e1ab59f6ad0b211076826365098f56
Offliner <42777325+Offliners@users.noreply.github.com>
2019-03-11 13:42:56 +0800

    Create a009.c

---
+++ 基礎題庫/Contents/a009/a009.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+    char input[9999];
+    while(scanf("%s",input) != EOF)
+    {
+         for(int i = 0;i<strlen(input);i++)
+              printf("%c",input[i]-7);
+         printf("\n");
+    }
+     return 0;
+}
