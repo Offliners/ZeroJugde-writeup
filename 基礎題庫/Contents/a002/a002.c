29626eece68251a3f45de4bfee66d66710418c6f
Offliner <42777325+Offliners@users.noreply.github.com>
2020-10-10 17:12:19 +0800

    Update a002.c

--- 基礎題庫/Contents/a002/a002.c
+++ 基礎題庫/Contents/a002/a002.c
@@ -2,8 +2,10 @@
 
 int main(void)
 {
-  int num1,num2;
-  while(scanf("%d %d",&num1,&num2) != EOF)
-    printf("%d\n",num1+num2);
-  return 0;
+	int num1, num2;
+	
+	while(scanf("%d %d", &num1, &num2) != EOF)
+		printf("%d\n", num1 + num2);
+	
+	return 0;
 }
