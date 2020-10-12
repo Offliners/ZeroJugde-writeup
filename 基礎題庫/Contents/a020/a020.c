9ee3e0ac18248fc45ddec57754b255a456ec6ae2
Offliner <42777325+Offliners@users.noreply.github.com>
2020-10-12 20:29:37 +0800

    Update a020.c

--- 基礎題庫/Contents/a020/a020.c
+++ 基礎題庫/Contents/a020/a020.c
@@ -1,92 +1,30 @@
 #include<stdio.h>
 
-int isReal(char []);
-int Sign2Num(char);
-
 int main(void)
 {
-	char id[10];
-	while(scanf("%s", id) != EOF)
+	int zone[26] = {10, 11, 12, 13, 14, 15, 16, 17, 34,
+	                18, 19, 20, 21, 22, 35, 23, 24, 25,
+					26, 27, 28, 29, 32, 30, 31, 33};
+	char id[11];
+	int total, i, j;
+	
+	while(scanf("%s", id)!=EOF)
 	{
-		if(isReal(id) % 10)
-			printf("fake\n");
-		else
+		total = 0;
+		total = zone[id[0]-'A'] % 10 * 9 + zone[id[0] - 'A'] / 10;
+		i = 1;
+		j = 8;
+		for(i = 1; i < 9; i++)
+		{
+			total += (id[i] - '0') * j;
+			j--;
+		}
+		total += id[i] - '0';
+		if(total % 10 == 0)
 			printf("real\n");
+		else
+			printf("fake\n");
 	}
 	
 	return 0;
-}
-
-int isReal(char str[10])
-{
-	int sum = 0;
-	int symbol, i;
-	
-	for(i = 1; i < 9; i++)
-		sum += (str[i] - '0') * (9 - i);
-	
-	sum += (str[9] - '0');
-	symbol = Sign2Num(str[0]);
-	sum +=  (symbol / 10  + (symbol % 10) * 9);
-	
-	return sum;
-}
-
-int Sign2Num(char S)
-{
-	switch(S)
-	{
-		case 'A':
-			return 10;
-		case 'B':
-			return 11;
-		case 'C':
-			return 12;
-		case 'D':
-			return 13;
-		case 'E':
-			return 14;
-		case 'F':
-			return 15;
-		case 'G':
-			return 16;
-		case 'H':
-			return 17;
-		case 'I':
-			return 34;
-		case 'J':
-			return 18;
-		case 'K':
-			return 19;
-		case 'L':
-			return 20;
-		case 'M':
-			return 21;
-		case 'N':
-			return 22;
-		case 'O':
-			return 35;
-		case 'P':
-			return 23;
-		case 'Q':
-			return 24;
-		case 'R':
-			return 25;
-		case 'S':
-			return 26;
-		case 'T':
-			return 27;
-		case 'U':
-			return 28;
-		case 'V':
-			return 29;
-		case 'W':
-			return 32;
-		case 'X':
-			return 30;
-		case 'Y':
-			return 31;
-		case 'Z':
-			return 33;
-	}
 }
