15c570894dc94d4d3ba64480b9155bd1229139bb
Offliner <42777325+Offliners@users.noreply.github.com>
2020-10-18 04:54:42 +0800

    Create f044.c

---
+++ 基礎題庫/Contents/f044/f044.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(void)
+{
+	int N, T;
+	
+	while(scanf("%d %d", &N, &T) != EOF)
+	{
+		int temp = T / N + 1;
+		int day;
+		
+		for(int i = 0; i < 17; i++)
+			if(pow(2, i) == temp)
+				day = i;
+				
+		printf("%d\n", day);
+	}
+	
+	return 0;
+}
