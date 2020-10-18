0b68a0a1f3e560e38a15326ed04175c23d8971bf
Offliner <42777325+Offliners@users.noreply.github.com>
2020-10-18 23:14:23 +0800

    Create c278.c

---
+++ 原創不分類題庫/Contnets/c278/c278.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int n;
+int data[100000];
+
+int cmp(const void *, const void *);
+
+int main(void)
+{	
+	while(scanf("%d", &n) != EOF)
+	{
+		int sum = 0;
+		
+		for(int i = 0; i < n; i++)
+			scanf("%d", &data[i]);
+			
+		qsort(data, n, sizeof(int), cmp);
+		
+		for(int i = 0; i < n; i += 2)
+			sum += data[i + 1] - data[i];
+		
+		printf("%d\n", sum);
+	}
+	
+	return 0;
+}
+
+int cmp(const void *a, const void *b)
+{
+    return *(const int*)a - *(const int*)b;
+}
+
+
+
