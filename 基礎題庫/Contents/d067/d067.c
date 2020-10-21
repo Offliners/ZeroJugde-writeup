a471ac1364b99af90ceed6be488f2b21018f35ad
Offliner <42777325+Offliners@users.noreply.github.com>
2020-10-22 03:22:33 +0800

    Create d067.c

---
+++ 基礎題庫/Contents/d067/d067.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int year;
+	
+	while(scanf("%d", &year) != EOF)
+	{
+		if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+			printf("a leap year\n");
+		else
+			printf("a normal year\n");
+	}
+	
+	return 0;
+}
