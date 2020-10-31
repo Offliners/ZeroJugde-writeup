670f927bd8b6a735299dc7a70d5f670f607b657b
Offliner <42777325+Offliners@users.noreply.github.com>
2020-10-31 16:55:05 +0800

    Create a225.c

---
+++ 基礎題庫/Contents/a225/a225.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+#define SWAP(x, y, t) ((t) = (x), (x) = (y), (y) = (t))
+
+int main(void)
+{
+	int n, temp;
+	int data[1000];
+	
+	while(scanf("%d", &n) != EOF)
+	{
+		for(int i = 0; i < n; i++)
+			scanf("%d", &data[i]);
+		
+		for(int i = 0; i < n - 1; i++)
+		{
+			for(int j = 0; j < n - 1; j++)
+			{
+				if(data[j] % 10 > data[j + 1] % 10)
+					SWAP(data[j], data[j + 1], temp);
+				else if(data[j] % 10 == data[j + 1] % 10)
+					if(data[j] < data[j + 1])
+						SWAP(data[j], data[j + 1], temp);
+			}
+		}
+		
+		for(int i = 0; i < n; i++)
+			printf("%d ", data[i]);
+		printf("\n");
+	}
+	
+	return 0;
+}
