3cb28d511c34ff40cd5dec4a96225dda3113b7c8
Offliner <42777325+Offliners@users.noreply.github.com>
2020-10-31 17:25:53 +0800

    Create a111.c

---
+++ UVa/Contents/a111/a111.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int n;
+	
+	while(1)
+	{
+		scanf("%d", &n);
+		
+		if(!n)
+			break;
+		
+		printf("%d\n", n * (n + 1) * (2 * n + 1) / 6);
+	}
+		
+		
+	return 0;
+}
