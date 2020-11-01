754e28a50c1ebe393ef1eb16ee23ec53ece9610f
Offliner <42777325+Offliners@users.noreply.github.com>
2020-11-01 21:18:24 +0800

    Create d323.c

---
+++ 原創不分類題庫/Contents/d323/d323.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int cmp(const void *, const void *);
+
+int main(void)
+{
+	int num;
+	int data[2000000];
+	
+	while(scanf("%d", &num) != EOF)
+	{
+		int i;
+		for(i = 0; i < num; i++)
+			scanf("%d", &data[i]);
+			
+		qsort(data, num, sizeof(int), cmp);
+		
+		for(i = 0; i < num; i++)
+			printf("%d ", data[i]);
+		printf("\n");
+	}
+	
+	return 0;
+}
+
+int cmp(const void *a, const void *b)
+{
+    return *(const int*)a - *(const int*)b;
+}
