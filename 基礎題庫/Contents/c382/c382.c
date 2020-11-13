7c4e3824020f40bf0ed3caa3fa6a50d2ef8f7ac2
Offliner <42777325+Offliners@users.noreply.github.com>
2020-11-13 18:43:56 +0800

    Create c382.c

---
+++ 基礎題庫/Contents/c382/c382.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(void)
+{
+	int a, b, result;
+	char op;
+	
+	while(scanf(" %d %c %d", &a, &op, &b) != EOF)
+	{
+		switch(op)
+		{
+			case '+':
+				result = a + b;
+				break;
+			case '-':
+				result = a - b;
+				break;
+			case '*':
+				result = a * b;
+				break;
+			case '/':
+				result = a / b;
+				break;
+		}
+		
+		printf("%d\n", result);
+	}
+	
+	return 0;
+}
