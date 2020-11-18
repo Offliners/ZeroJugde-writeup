5fc56187cf8befad262b5a41e4a964f0d0ffe300
Offliner <42777325+Offliners@users.noreply.github.com>
2020-11-19 01:19:55 +0800

    Create e926.cpp

---
+++ 基礎題庫/Contents/e926/e926.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cout << "|\'o\'|\n" << endl;
+	cout << "\\^_^/\n" << endl;
+	cout << "(\"o\")\n" << endl;
+	cout << "[-%-]\n" << endl;
+	
+	return 0;
+}
