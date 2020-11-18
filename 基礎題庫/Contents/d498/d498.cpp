52f1a3c77502b89a65bb7a1905d64bee2f62a329
Offliner <42777325+Offliners@users.noreply.github.com>
2020-11-19 01:09:54 +0800

    Create d498.cpp

---
+++ 基礎題庫/Contents/d498/d498.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int n;
+	
+	while(cin >> n)
+		for(int i = 0; i < n; i++)
+			cout << "I don't say swear words!" << endl;
+	
+	return 0;
+} 
