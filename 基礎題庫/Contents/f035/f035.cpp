5ced8e51bd1575ed0032c1cdc59a1943069a0b6f
Offliner <42777325+Offliners@users.noreply.github.com>
2020-11-19 21:25:31 +0800

    Create f035.cpp

---
+++ 基礎題庫/Contents/f035/f035.cpp
@@ -0,0 +1,22 @@
+## C++
+```C++
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	string s;
+	
+	while(getline(cin, s))
+	{
+		for(int i = 0; i < s.length() - 1; i++)
+			cout << int(s[i]) << "_";
+		
+		cout << int(s[s.length() - 1]) << endl;
+	}
+	
+	return 0;
+}
+```
+ * Result : `AC (2ms, 328KB)`
