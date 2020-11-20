e2410d80b05d71bacc7c48bf46df36b574332307
Offliner <42777325+Offliners@users.noreply.github.com>
2020-11-21 00:12:14 +0800

    Create c726.cpp

---
+++ 基礎題庫/Contents/c726/c726.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	string name1;
+	string name2;
+	
+	while(cin >> name1 >> name2)
+		cout << name1 << " and " << name2 << " sitting in the tree" << endl;
+	
+	return  0;
+}
