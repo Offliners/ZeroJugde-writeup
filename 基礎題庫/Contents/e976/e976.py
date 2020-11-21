e3a9efe170613c143d7108990f55cc17381a774f
Offliner <42777325+Offliners@users.noreply.github.com>
2020-11-21 16:04:47 +0800

    Create e976.py

---
+++ 基礎題庫/Contents/e976/e976.py
@@ -0,0 +1,12 @@
+import sys
+
+for temp in sys.stdin:
+    try:
+        h, m, s = map(int, temp.strip().split())
+    except:
+        continue
+    
+    if h * s >= m:
+        print(h, m, str(s) + ".", "I will make it!")
+    else:
+        print(h, m, str(s) + ".", "I will be late!")
