eb80ccaa415de3a43765c3ea9afbece0023f1fdb
Offliner <42777325+Offliners@users.noreply.github.com>
2020-11-21 16:07:53 +0800

    Update e976.py

--- 基礎題庫/Contents/e976/e976.py
+++ 基礎題庫/Contents/e976/e976.py
@@ -2,10 +2,10 @@ import sys
 
 for temp in sys.stdin:
     try:
-        h, m, s = map(int, temp.strip().split())
+        h, m, s = map(int, temp.split())
     except:
         continue
-    
+
     if h * s >= m:
         print(h, m, str(s) + ".", "I will make it!")
     else:
