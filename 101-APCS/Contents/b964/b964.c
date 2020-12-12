7353342eab3556da156ebf7c710288634f2386b5
Offliner <42777325+Offliners@users.noreply.github.com>
2020-12-13 03:25:31 +0800

    Update b964.c

--- 101-APCS/Contents/b964/b964.c
+++ 101-APCS/Contents/b964/b964.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 
 int cmp(const void *, const void *);
+void isFail(int, int*);
 
 int main(void)
 {
@@ -15,28 +16,7 @@ int main(void)
 			
 		qsort(score, n, sizeof(int), cmp);
 		
-		int fail = 0;
-		for(int i = 0; i < n - 1; i++)
-		{
-			printf("%d ", score[i]);
-			
-			if(score[i] < 60)
-				fail++;
-		}
-		printf("%d\n", score[n-1]);
-		
-		if(score[n-1] < 60)
-			fail++;
-		
-		if(!fail)
-			printf("best case\n");
-		else
-			printf("%d\n", score[fail-1]);
-			
-		if(fail == n)
-			printf("worst case\n");
-		else
-			printf("%d\n", score[fail]);
+		isFail(n, score);
 	}
 	
 	return 0;
@@ -45,4 +25,30 @@ int main(void)
 int cmp(const void *a, const void *b)
 {
     return *(const int*)a - *(const int*)b;
+}
+
+void isFail(int n, int score[])
+{
+	int fail = 0;
+	for(int i = 0; i < n - 1; i++)
+	{
+		printf("%d ", score[i]);
+
+		if(score[i] < 60)
+			fail++;
+	}
+	printf("%d\n", score[n-1]);
+
+	if(score[n-1] < 60)
+		fail++;
+
+	if(!fail)
+		printf("best case\n");
+	else
+		printf("%d\n", score[fail-1]);
+
+	if(fail == n)
+		printf("worst case\n");
+	else
+		printf("%d\n", score[fail]);
 }
