b098647d3d7b91e07e98b54f3e18b6535e53a508
Offliner <42777325+Offliners@users.noreply.github.com>
2021-01-12 00:33:41 +0800

    Create d581.cpp

---
+++ 原創不分類題庫/Contents/d581/d581.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+	long long int n;
+	
+	while(cin >> n)
+		for(long long int i = 0; i < n; i++)
+			cout << "=_=|||" << endl;
+	
+	return 0;
+}
